cd70bddb803da50cbadc385643db45507061f400
mml78 <michele.marques-leben@edu.esiee-it.fr>
2026-04-30 19:53:18 +0300

    Create task_animals.py

---
+++ tasks/tasks/task_animals.py
@@ -0,0 +1,6 @@
+###### Task ######
+#
+# Create an Animal base class with a method make_sound()
+# Subclasses: Dog, Cat, Bird with custom implementations of make_sound()
+# Instantiate each and call make_sound()
+##################
